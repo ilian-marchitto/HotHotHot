00024c09e3699771c78936a12b50d93717d902e3
burbeck <burbeck.heather@etu.univ-amu.fr>
2026-03-30 10:07:17 +0200

    création du fichier de config des paths

---
+++ config.js
@@ -0,0 +1,24 @@
+
+
+export const APP_CONFIG = {
+    PATHS: {
+        ASSETS: {
+            CSS: '/assets/css/',
+            IMG: '/assets/img/',
+            ICONS: '/assets/img/icons/',
+            DATA: '/assets/data/'
+        },
+
+        SRC: {
+            VIEWS: '/src/views/',
+            COMPONENTS: '/src/components/',
+            SERVICES: '/src/services/',
+            CORE: '/src/core/'
+        },
+        ROOT: '/'
+    },
+
+    ENDPOINTS: {
+        MESURES_CAPTEURS: '/assets/data/exempleJson.json'
+    }
+};
